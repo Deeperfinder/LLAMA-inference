97f8e004c886406433ae2d6558641bd89bb338bb
xf <xf02145331@alibaba-inc.com>
2025-01-20 09:52:36 +0000

    update annotation

--- tools/export.py
+++ tools/export.py
@@ -541,8 +541,8 @@ def load_hf_model(model_path):
 
     # convert LlamaConfig to ModelArgs
     config = ModelArgs()
-    if any(['config.json' in path for path in os.listdir("/home/modelscope/TinyLlama-1.1B-Chat-v1.0")]):
-        with open(os.path.join("/home/modelscope/TinyLlama-1.1B-Chat-v1.0", 'config.json'), 'r') as f:
+    if any(['config.json' in path for path in os.listdir("/nfs/xf/llama2_model/Llama-2-7b-hf")]):
+        with open(os.path.join("/nfs/xf/llama2_model/Llama-2-7b-hf", 'config.json'), 'r') as f:
             config_json = json.load(f)
         config.dim = config_json["hidden_size"]
         config.n_layers = config_json["num_hidden_layers"]
