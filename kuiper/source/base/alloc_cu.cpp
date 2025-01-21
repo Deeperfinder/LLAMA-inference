ec25d96c6cb5934a37583b16b138543fef5dbd94
xf <xf02145331@alibaba-inc.com>
2025-01-21 07:01:42 +0000

    add annotation

--- kuiper/source/base/alloc_cu.cpp
+++ kuiper/source/base/alloc_cu.cpp
@@ -11,6 +11,7 @@ void* CUDADeviceAllocator::allocate(size_t byte_size) const {
   if (byte_size > 1024 * 1024) {
     auto& big_buffers = big_buffers_map_[id];
     int sel_id = -1;
+    // 在map为cuda device id的buffer vector找到最合适的内存buffer
     for (int i = 0; i < big_buffers.size(); i++) {
       if (big_buffers[i].byte_size >= byte_size && !big_buffers[i].busy &&
           big_buffers[i].byte_size - byte_size < 1 * 1024 * 1024) {
@@ -23,7 +24,7 @@ void* CUDADeviceAllocator::allocate(size_t byte_size) const {
       big_buffers[sel_id].busy = true;
       return big_buffers[sel_id].data;
     }
-
+    // big_buffer vector需要初始化
     void* ptr = nullptr;
     state = cudaMalloc(&ptr, byte_size);
     if (cudaSuccess != state) {
@@ -38,7 +39,8 @@ void* CUDADeviceAllocator::allocate(size_t byte_size) const {
     big_buffers.emplace_back(ptr, byte_size, true);
     return ptr;
   }
-
+  // 小内存 < 1MB
+  // 不存在-1的时候，会构造一个空的std::vector<CudaMemoryBuffer>
   auto& cuda_buffers = cuda_buffers_map_[id];
   for (int i = 0; i < cuda_buffers.size(); i++) {
     if (cuda_buffers[i].byte_size >= byte_size && !cuda_buffers[i].busy) {
@@ -71,6 +73,7 @@ void CUDADeviceAllocator::release(void* ptr) const {
   }
   cudaError_t state = cudaSuccess;
   for (auto& it : cuda_buffers_map_) {
+    // 判断每个设备上的no_bysy_cnt是否大于1GB, 如果是，释放其内存
     if (no_busy_cnt_[it.first] > 1024 * 1024 * 1024) {
       auto& cuda_buffers = it.second;
       std::vector<CudaMemoryBuffer> temp;
@@ -89,7 +92,8 @@ void CUDADeviceAllocator::release(void* ptr) const {
       no_busy_cnt_[it.first] = 0;
     }
   }
-
+  // 如果这个要release的ptr在任意一个设备中的cuda_buffer_map_ 和 big_buffer_map_中存在
+  // 更新no_busy_cnt 和 该buffer busy状态，不用free 该块内存
   for (auto& it : cuda_buffers_map_) {
     auto& cuda_buffers = it.second;
     for (int i = 0; i < cuda_buffers.size(); i++) {
@@ -107,6 +111,7 @@ void CUDADeviceAllocator::release(void* ptr) const {
       }
     }
   }
+  // 否则释放其内存
   state = cudaFree(ptr);
   CHECK(state == cudaSuccess) << "Error: CUDA error when release memory on device";
 }
